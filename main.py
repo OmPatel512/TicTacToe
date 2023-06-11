e3ec6ca60e15da3b4f977a3e100394cb18701ea2
om patel <omsuperson@gmail.com>
2023-06-11 21:11:09 +0530

    Extra spaced lines eliminated

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ currentPlayer = "X"
 winner = None
 gameRunning = True
 
-
 def printBoard(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
     print("----------")
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("----------")
     print(board[6] + " | " + board[7] + " | " + board[8])
-
-
 def playerInput(board):
     inp = int(input("Enter a number between 1-9: "))
     if inp >= 1 and inp <= 9 and board == '-':
